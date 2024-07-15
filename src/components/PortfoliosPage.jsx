5fef6b27f1b78bf79351999e2e6daa953a054a39
Ariels875 <ascastro875@gmail.com>
2024-07-14 19:16:43 -0500

    Added links to portfolios

--- src/components/PortfoliosPage.jsx
+++ src/components/PortfoliosPage.jsx
@@ -1,6 +1,10 @@
 import '../styles/PortfoliosPage.css';
 
 function PortfoliosPage() {
+
+  const openInNewTab = (url) => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
   return (
     <div className="portfolios-page">
       <h1>PORTAFOLIOS</h1>
@@ -9,32 +13,22 @@ function PortfoliosPage() {
           <img src="https://i.imgur.com/fli08dk.png" alt="Ariel Castro" />
           <h2>ARIEL CASTRO</h2>
           <p>Desarrollador especializado en el frontend</p>
-          <button className="btn" href="#">
+          <button
+            className="btn"
+            onClick={() => openInNewTab('https://arielspage.pages.dev/')}
+          >
             <strong>Ver Portfolio</strong>
-            <div id="container-stars">
-              <div id="stars"></div>
-            </div>
-
-            <div id="glow">
-              <div className="circle"></div>
-              <div className="circle"></div>
-            </div>
           </button>
         </div>
         <div className="portfolio">
           <img src="https://i.imgur.com/6900Hq9.png" alt="Cristian Herrera" />
           <h2>CRISTIAN HERRERA</h2>
           <p>Desarrollador especializado en el backend</p>
-          <button className="btn" href="#">
+          <button
+            className="btn"
+            onClick={() => openInNewTab('https://cristianpage.pages.dev/')}
+          >
             <strong>Ver Portfolio</strong>
-            <div id="container-stars">
-              <div id="stars"></div>
-            </div>
-
-            <div id="glow">
-              <div className="circle"></div>
-              <div className="circle"></div>
-            </div>
           </button>
         </div>
       </div>
